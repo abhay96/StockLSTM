6dc5b3c4c329ca1e84217c9fc5fd80ee1e63b796
abhay96 <52976099+abhay96@users.noreply.github.com>
2022-07-02 10:36:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandas_datareader as pdr
-from keras.models import load_model 
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
